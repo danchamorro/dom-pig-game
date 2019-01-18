8b4d157bcebe746a2aa8f27a1de233dce122342b
Dan Chamorro <dan@danchamorro.com>
2019-01-18 17:43:41 -0500

    Add chging of dice image when roll dice is clicked.

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".dice").style.display = "none";
 
-document.querySelector(`#current-${activePlayer} `).textContent = dice;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1. Random Number
+  let dice = Math.floor(Math.random() * 6) + 1;
+  // 2. Display the results
+  let diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = `/img/dice-${dice}.png`;
+  // Update the round score IF the rolled number was NOT a 1
+});
